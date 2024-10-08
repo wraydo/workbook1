13121cb39afb606780df691c68320be628c2669f
wraydo <phillipdowner01@gmail.com>
2024-10-08 10:35:13 -0400

    hello first program exaple

---
+++ hello.js
@@ -0,0 +1,8 @@
+// example 1: declare and assign seperately
+var greeting = "Hello"; 
+
+console.log(greeting)
+// example 2:
+// declare and assiign on the same line
+var greetings2 = "Programming is fun"
+console.log (greetings2)
