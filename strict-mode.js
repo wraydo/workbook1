10fc3540e26fef08b057f5f5ae9d1269692889ff
wraydo <phillipdowner01@gmail.com>
2024-10-08 13:17:13 -0400

    excercise 1-38-2

--- strict-mode.js
+++ strict-mode.js
@@ -4,3 +4,40 @@ var number;
 number = 10;
 number = 12;
 console.log(number);
+
+var customid = "4499"
+console.log(customid)
+
+var personFirstName = "Jim";
+console.log(personFirstName);
+
+var middleName = "John";
+console.log(middleName);
+
+var lastName = "Cooks";
+console.log(lastName);
+
+var gender = "Male";
+console.log(gender);
+
+var dateOfBirth = "07/01/1990";
+console.log(dateOfBirth);
+
+var driversls = "QQ11114";
+console.log(driversls);
+
+var autopolicyn = "44221188";
+console.log(autopolicyn);
+
+// Excercise3
+
+var firstName;
+firstName = "Wray";
+
+var city;
+city = "Queens"
+
+var state 
+state = "NY"
+
+console.log(firstName,city,state)
